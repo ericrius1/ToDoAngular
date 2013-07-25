afb84a05cacc425709b8af5dfc697d140f7af516
Eric <Ericrius1@gmail.com>
2013-07-25 15:23:42 -0700

    init

---
+++ todo.js
@@ -0,0 +1,25 @@
+function TodoCtrl($scope) {
+  $scope.todos = [
+    {text:'Complete To-Do Application', done:true}];
+ 
+  $scope.addTodo = function() {
+    $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.todoText = '';
+  };
+ 
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+ 
+  $scope.delete = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+}
